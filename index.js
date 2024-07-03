b07e522b276727e8678a471d0f980e9388af15b0
kananarora1 <arorakanan63@gmail.com>
2024-07-03 11:15:25 +0530

    Added post, delete, put and get calls

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
 const express = require('express');
 const app = express();
 
-let courses = {
-    1: {
+app.use(express.json());
+
+let courses = [
+     {
         id: 1,
         name: "java"
     },
-    2: {
+     {
         id: 2,
         name: "javascript"
     },
-    3: {
+     {
         id: 3,
         name: "python"
     }
-};
+];
 
 app.get('/courses', (req, res) => {
     res.json(courses);
 });
+
+app.post('/post', (req, res) => {
+    let course = {
+        id: courses.length + 1,
+        name: req.body.name
+    
+    };
+    courses.push(course);
+    res.send(courses);
+});
+
+app.put('/put/:id', (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/delete/:id', (req, res) => {  
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    let index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.send(course);
+});
+
+function middleware(req, res, next) {
+    console.log('called');
+    next();
+}
+
+//logger
+// method, ip, hostname, date
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
